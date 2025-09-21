f424d96bfdefa4782fd87740505d95a46490648e
AhmedAlkhaldi93 <ahmed.el-khaldi@hotmail.com>
2025-09-21 15:05:27 +0200

    21-09-2025 update

---
+++ expenses_tracker_frontend/app/dashboard/expenses/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Expenses() {
+    return(
+        <div>
+            expenses page!
+        </div>
+    );
+};
---
+++ expenses_tracker_frontend/app/dashboard/income/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Income() {
+    return(
+        <div>
+            <center>
+            <h1> Income Page</h1>
+            </center> 
+        </div>
+    );
+};
